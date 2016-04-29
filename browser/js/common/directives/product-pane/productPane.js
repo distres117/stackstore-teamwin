68b8d700f823465f9efddc5e6658a6d96505d579
flexjames <james.sh.ma@gmail.com>
2016-04-28 22:59:04 -0400

    ngToast implemented

--- browser/js/common/directives/product-pane/productPane.js
+++ browser/js/common/directives/product-pane/productPane.js
@@ -3,11 +3,16 @@ app.directive('productPane', function(){
     scope:{
       product: '='
     },
-    controller: function($scope, CartFactory){
+    controller: function($scope, CartFactory, ngToast){
       $scope.getStars = function(product){
           return new Array(product.stars);
       };
       $scope.addToCart = function(product){
+        ngToast.success({
+          content: product.name + ' added to the cart!',
+          timeout: 4000,
+          dismissOnTimeout: true
+        });
         return CartFactory.addToCart(product, 1);
       };
     },
